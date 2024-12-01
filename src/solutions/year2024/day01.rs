d42e82efdec47b3fdaddc44fa4fe54f2d8d1a35b
literally-sai <173792483+literally-sai@users.noreply.github.com>
2024-12-01 17:20:21 +0100

    add part2

--- src/solutions/year2024/day01.rs
+++ src/solutions/year2024/day01.rs
@@ -1,24 +1,22 @@
-use std::{error::Error, fs::read_to_string, path::Path};
+use std::{collections::HashMap, error::Error, fs::read_to_string};
 
-use crate::utils::Input;
+use crate::utils::Input::{Input, get_input};
 
-pub fn solve(input: &Input) -> Result<String, Box<dyn Error>> {
-    
-    let content: String;
+pub fn solve(input: (Input, Input)) -> Result<(String, String), Box<dyn Error>> {
+    let output_1 = part_01(input.0)?;
+    let output_2 = part_02(input.1)?;
 
-    match input {
-        Input::Path(ref path) => {
-            content = read_to_string(path)?;
-        }
-        Input::Text(s) => {
-            content = s.clone();
-        }
-    }
+    Ok((output_1, output_2))
+}
 
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: Vec<i32> = Vec::new();
 
-    for line in content.lines() {
+fn part_01(input:  Input) -> Result<String, Box<dyn Error>> {
+    let input = get_input(input)?; 
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in input.lines() {
         let mut numbers = line.split_whitespace();
         if let (Some(left_num), Some(right_num)) = (numbers.next(), numbers.next()) {
             left.push(left_num.parse()?);
@@ -26,15 +24,48 @@ pub fn solve(input: &Input) -> Result<String, Box<dyn Error>> {
         }
     }
 
+    if left.len() != right.len() {
+        return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Mismatched input lengths"
+        )));
+    }
+
     left.sort();
     right.sort();
 
-    let mut distance: i32 = 0;
+    let distance: i32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| (r - l).abs())
+        .sum();
 
-    for i in 0..right.len() {
-       distance += i32::abs(right[i] - left[i]);
+    Ok(distance.to_string())
+}
 
+fn part_02(input: Input) -> Result<String, Box<dyn Error>> {
+    let input = get_input(input)?;
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in input.lines() {
+        let mut numbers = line.split_whitespace();
+        if let (Some(left_num), Some(right_num)) = (numbers.next(), numbers.next()) {
+            left.push(left_num.parse()?);
+            right.push(right_num.parse()?);
+        }
     }
 
-    Ok(distance.to_string()) 
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    for num in right {
+        *map.entry(num).or_insert(0) += 1; 
+    }
+
+    let sum = left.iter()
+        .filter_map(|l| map.get(l).map(|r| l* r))
+        .sum();
+
+    Ok(sum.to_string())
 }
