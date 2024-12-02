c4acc75e62b054255dd77da580f3509c1f4514de
literally-sai <173792483+literally-sai@users.noreply.github.com>
2024-12-02 15:35:57 +0100

    function parameter change

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ pub mod solutions;
 pub mod utils;
 
 use crate::utils::input::Input;
-pub fn solve(year: u32, day: u32, input: (Input, Input)) -> Result<(String, String), Box<dyn Error>> {
+use crate::utils::output::Output;
+pub fn solve(year: u32, day: u32, input: Input) -> Result<Output, Box<dyn Error>> {
     match year {
         2024 => solutions::year2024::solve_day(day, input),
         _ => Err(Box::new(std::io::Error::new(ErrorKind::InvalidInput, "Unsupported year",))),
