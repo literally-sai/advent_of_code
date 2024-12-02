97117450c108243846fdc261436f65df1bd362e0
literally-sai <173792483+literally-sai@users.noreply.github.com>
2024-12-02 15:34:36 +0100

    restructured tests

--- tests/year2024.rs
+++
@@ -1,9 +0,0 @@
-use advent_of_code::solutions::year2024;
-use advent_of_code::utils::input::Input;
-#[test]
-fn day01() {
-    let solution = year2024::solve_day(
-        1,
-        (Input::Path("inputs/2024/day01-1.txt".into()), Input::Path("inputs/2024/day01-2.txt".into()))).unwrap();
-    eprintln!("Part1: {}\nPart2: {}", solution.0, solution.1);
-}
