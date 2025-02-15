b25a00fd7e98de573dbe455d593badf1228bd299
literally-sai <173792483+literally-sai@users.noreply.github.com>
2025-02-15 23:41:41 +0200

    Add year2015

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
-use std::{error::Error, io::ErrorKind};
-pub mod solutions;
 pub mod utils;
-
-use crate::utils::input::Input;
-use crate::utils::output::Output;
-pub fn solve(year: u32, day: u32, input: Input) -> Result<Output, Box<dyn Error>> {
-    match year {
-        2024 => solutions::year2024::solve_day(day, input),
-        _ => Err(Box::new(std::io::Error::new(ErrorKind::InvalidInput, "Unsupported year",))),
-    }
-}
+pub mod year2015;
