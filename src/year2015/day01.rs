285f1813f7848c7926294f4f6ae1bf4aa920ad8a
literally-sai <173792483+literally-sai@users.noreply.github.com>
2025-02-16 15:58:45 +0200

    2015 day01 solved

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -1,12 +1,28 @@
 pub fn part1(input: &str) -> i32 {
     input.chars()
-        .map(|c| {
-            match c {
+        .map(|step| {
+            match step {
                 '(' => 1,
                 ')' => -1,
                 _ => 0
             }
         }).fold(0, |acc, step| acc + step)
+}
+
+pub fn part2(input: &str) -> i32 {
+    let mut floor = 0;
+    for (idx, step) in input.chars().enumerate() {
+        match step {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => ()
+        }
+        if floor == -1 {
+            let idx = idx as i32 + 1;
+            return idx;
+        }
+    }
+    input.len() as i32
 }
 
 
@@ -15,19 +31,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_examples() {
+    fn test_a() {
         assert_eq!(part1("(())"), 0);
         assert_eq!(part1("()()"), 0);
+    }
 
+    #[test]
+    fn test_b() {
         assert_eq!(part1("((("), 3);
         assert_eq!(part1("(()(()("), 3);
+    }
 
+    #[test]
+    fn test_c() {
         assert_eq!(part1("))((((("), 3);
+    }
 
+    #[test]
+    fn test_d() {
         assert_eq!(part1("())"), -1);
         assert_eq!(part1("))("), -1);
+    }
 
+    #[test]
+    fn test_e() {
         assert_eq!(part1(")))"), -3);
+        assert_eq!(part1(")())())"), -3)
+    }
+
+    #[test]
+    fn test_f() {
+        assert_eq!(part2(")"), 1);
+    }
+
+    #[test]
+    fn test_g() {
+        assert_eq!(part2("()()))"), 5);
     }
 
     #[cfg(feature = "inputfile")]
@@ -35,6 +74,14 @@ mod tests {
     fn part1_inputfile() {
         use crate::utils::read_input::read_input;
         let input = read_input(2015, 1).expect("Failed to read input file");
-        println!("The output for 2015 day01 is {}", part1(&input));
+        println!("The output for 2015 day01 part1 is {}", part1(&input));
+    }
+
+    #[cfg(feature = "inputfile")]
+    #[test]
+    fn part2_inputfile() {
+        use crate::utils::read_input::read_input;
+        let input = read_input(2015, 1).expect("Failed to read input file");
+        println!("The output for 2015 day01 part2 is {}", part2(&input))
     }
 }
